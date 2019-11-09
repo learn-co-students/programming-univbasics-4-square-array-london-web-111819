5d54238b0d7002e899da474a6769031149be5058
keemo <keemowatson1@gmail.com>
2019-11-09 14:42:04 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,19 @@
+# def square_array(array)
+# count = 0
+# new_array = []
+#   while array [count] do
+#     new_array << array [count] ** 2
+#      #return new_array
+#      count += 1
+#   end
+# end
+
 def square_array(array)
-  # your code here
+count = 0
+new_array = []
+  while array [count] do
+    new_array.push(array [count] ** 2)
+     count += 1
+  end
+  return new_array
 end
